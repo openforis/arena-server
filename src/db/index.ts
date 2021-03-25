191d499f456d90362ba93473eb133dfe9eed78dd
sorja <ping@mirosorja.com>
2021-03-25 13:00:39 +0200

    ProcessEnv: Init

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,18 +1,5 @@
 import * as pgPromise from 'pg-promise'
-
-// TODO: Add ProcessUtils
-const ProcessUtils = {
-  ENV: {
-    debug: true,
-    pgSsl: true,
-    dbUrl: '',
-    pgUser: '',
-    pgDatabase: '',
-    pgPassword: '',
-    pgHost: '',
-    pgPort: 0,
-  },
-}
+import { ProcessEnv } from 'src/processEnv'
 
 // TODO: Add logging
 const logger = {
@@ -29,7 +16,7 @@ const debugOptions = {
 }
 
 const initOptions = {
-  ...(ProcessUtils.ENV.debug ? debugOptions : {}),
+  ...(ProcessEnv.debug ? debugOptions : {}),
 }
 
 const pgp = pgPromise(initOptions)
@@ -44,20 +31,20 @@ const configCommon = {
   // Max number of clients in the pool
   max: 10,
   // Whether to use ssl connections
-  ssl: ProcessUtils.ENV.pgSsl,
+  ssl: ProcessEnv.pgSsl,
 }
 
-const config = ProcessUtils.ENV.dbUrl
+const config = ProcessEnv.dbUrl
   ? {
-      connectionString: ProcessUtils.ENV.dbUrl,
+      connectionString: ProcessEnv.dbUrl,
       ...configCommon,
     }
   : {
-      user: ProcessUtils.ENV.pgUser,
-      database: ProcessUtils.ENV.pgDatabase,
-      password: ProcessUtils.ENV.pgPassword,
-      host: ProcessUtils.ENV.pgHost,
-      port: ProcessUtils.ENV.pgPort,
+      user: ProcessEnv.pgUser,
+      database: ProcessEnv.pgDatabase,
+      password: ProcessEnv.pgPassword,
+      host: ProcessEnv.pgHost,
+      port: ProcessEnv.pgPort,
       ...configCommon,
     }
 
