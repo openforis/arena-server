65c42e705cbab91ae395992f5653675188775f5a
minotogna <mino.togna@gmail.com>
2021-04-02 08:24:08 +0200

    add SQL/Tables to export

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 export { ArenaServer } from './arenaServer'
 
-export { DB, DBMigrator, Schemata } from './db'
+export { DB, DBMigrator, SqlSelectBuilder, SQLs, Schemata, TableNodeDef, TableSurvey } from './db'
+export type { BaseProtocol } from './db'
 
 export { Logger } from './log'
 
