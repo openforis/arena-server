5058786c358efe78209bdc822f72c948e461a6c4
minotogna <mino.togna@gmail.com>
2021-04-06 09:44:21 +0200

    fix SurveyRepository import

--- src/service/survey/getAllIds.ts
+++ src/service/survey/getAllIds.ts
@@ -1,5 +1,5 @@
 import { SurveyService } from '@openforis/arena-core'
 
-import { SurveyRepository } from '../../repository/survey'
+import { SurveyRepository } from '../../repository'
 
 export const getAllIds: SurveyService['getAllIds'] = SurveyRepository.getAllIds
