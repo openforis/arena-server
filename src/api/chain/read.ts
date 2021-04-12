cb9e0e8c4e19f11eca678b77caa1e2d8a5805619
minotogna <mino.togna@gmail.com>
2021-04-12 09:35:32 +0200

    fix deepscan

--- src/api/chain/read.ts
+++ src/api/chain/read.ts
@@ -5,12 +5,12 @@ import { ApiEndpoint } from '../endpoint'
 
 export const ChainRead: ExpressInitializer = {
   init: (express: Express): void => {
-    express.get(ApiEndpoint.chain.chainsCount(':surveyId'), (_req, _res, next) => {
-      try {
-        //TODO
-      } catch (error) {
-        next(error)
-      }
+    express.get(ApiEndpoint.chain.chainsCount(':surveyId'), (_req, _res, _next) => {
+      //TODO
+      // try {
+      // } catch (error) {
+      //   next(error)
+      // }
     })
   },
 }
