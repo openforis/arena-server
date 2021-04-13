759df3af91d4d4c0e3fbf7237deb37a3e4d5bc1f
minotogna <mino.togna@gmail.com>
2021-04-13 07:36:15 +0200

    use Objects.propertyOf

--- src/repository/chain/count.ts
+++ src/repository/chain/count.ts
@@ -1,8 +1,5 @@
-import { ChainProps } from '@openforis/arena-core'
+import { ChainProps, Objects } from '@openforis/arena-core'
 import { DB, SqlSelectBuilder, TableChain } from '../../db'
-
-//TODO: I will add this later to '@openforis/arena-core'.Objects
-const propertyOf = <T>(name: keyof T): keyof T => name
 
 export const count = (options: { cycle: string; surveyId: number }, client = DB): Promise<number> => {
   const { cycle, surveyId } = options
@@ -11,7 +8,7 @@ export const count = (options: { cycle: string; surveyId: number }, client = DB)
   const sql = new SqlSelectBuilder()
     .select(`count(${table.alias}.*)`)
     .from(table)
-    .where(`(${table.props})->'${propertyOf<ChainProps>('cycles')}' @> '"${cycle}"'`)
+    .where(`(${table.props})->'${Objects.propertyOf<ChainProps>('cycles')}' @> '"${cycle}"'`)
     .build()
 
   return client.one<number>(sql, [], (res) => res.count)
