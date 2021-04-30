792537c18b354c16f5607dcb05132f84fdbb017f
sorja <ping@mirosorja.com>
2021-04-30 10:26:28 +0300

    repository/survey/get: support draft, backup

--- src/repository/survey/get.ts
+++ src/repository/survey/get.ts
@@ -10,6 +10,8 @@ import { DBs } from '../../db/dbs/index'
 export const get = async (
   options: {
     surveyId: number
+    draft?: boolean
+    backup?: boolean
   },
   client: BaseProtocol = DB
 ): Promise<Survey> => {
@@ -30,5 +32,9 @@ export const get = async (
     .where(`${table.id} = $1`)
     .build()
 
-  return client.one<Survey>(sql, [options.surveyId], (row) => DBs.transformCallback({ row }))
+  const { draft, backup } = options
+
+  return client.one<Survey>(sql, [options.surveyId], (row) =>
+    DBs.transformCallback({ row, assocPublishedDraft: false, draft, backup })
+  )
 }
