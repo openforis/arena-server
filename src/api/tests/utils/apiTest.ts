3e81d0fa10b8babe5c51c701ca5e16c4330bd468
sorja <ping@mirosorja.com>
2021-05-07 21:25:33 +0300

    apiTest/utils: add cookie

--- src/api/tests/utils/apiTest.ts
+++ src/api/tests/utils/apiTest.ts
@@ -3,16 +3,17 @@ import request, { SuperTest, Test } from 'supertest'
 
 export class ApiTest {
   private readonly superTest: SuperTest<Test>
+  public cookie = ''
 
   constructor(server: Server) {
     this.superTest = request(server)
   }
 
   public get(url: string): Test {
-    return this.superTest.get(url).set('Accept', 'application/json')
+    return this.superTest.get(url).set('Accept', 'application/json').set('cookie', this.cookie)
   }
 
   public post(url: string): Test {
-    return this.superTest.post(url).set('Accept', 'application/json')
+    return this.superTest.post(url).set('Accept', 'application/json').set('cookie', this.cookie)
   }
 }
