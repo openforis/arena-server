d9fd5ca87c2bb363a4aa4bc5b23d1e57bc9f4145
sorja <ping@mirosorja.com>
2021-05-12 13:08:40 +0300

    survey/repository: create

--- src/repository/survey/create.ts
+++ src/repository/survey/create.ts
@@ -1,5 +1,5 @@
 import { Survey } from '@openforis/arena-core'
-import { BaseProtocol, DB, DBs, SqlSelectBuilder, TableSurvey } from '../../db'
+import { BaseProtocol, DB, DBs, SqlInsertBuilder, TableSurvey } from '../../db'
 
 /**
  * Returns a list of surveys by name.
@@ -8,25 +8,31 @@ import { BaseProtocol, DB, DBs, SqlSelectBuilder, TableSurvey } from '../../db'
  * @param client - Database client.
  */
 
-export const create = (options: { survey: Survey }, client: BaseProtocol = DB): Promise<Survey> => {
-  // TODO
-  //
-  // export const insertSurvey = async ({ survey, props = {}, propsDraft = {} }, client = db) =>
-  //   client.one(
-  //     `
-  //       INSERT INTO survey (uuid, props, props_draft, owner_uuid, published, draft, template )
-  //       VALUES ($1, $2, $3, $4, $5, $6, $7)
-  //       RETURNING ${surveySelectFields()}
-  //     `,
-  //     [
-  //       Survey.getUuid(survey),
-  //       props,
-  //       propsDraft,
-  //       survey.ownerUuid,
-  //       Survey.isPublished(survey),
-  //       Survey.isDraft(survey),
-  //       Survey.isTemplate(survey),
-  //     ],
-  //     (def) => DB.transformCallback(def, true)
-  //   )
+export const create = async (
+  options: { survey: Survey; propsDraft?: any; props?: any },
+  client: BaseProtocol = DB
+): Promise<Survey> => {
+  const { survey, props = {}, propsDraft = {} } = options
+  const table = new TableSurvey()
+  const columns = [
+    table.uuid,
+    table.props,
+    table.propsDraft,
+    table.ownerUuid,
+    table.published,
+    table.draft,
+    table.template,
+  ]
+
+  const sql = new SqlInsertBuilder()
+    .insertInto(table, ...columns)
+    .values('$1', '$2', '$3', '$4', '$5', '$6', '$7')
+    .returning(...columns, table.dateCreated, table.dateCreated)
+    .build()
+
+  return client.one<Survey>(
+    sql,
+    [survey.uuid, props, propsDraft, survey.ownerUuid, survey.published, survey.draft, survey.template],
+    (def) => DBs.transformCallback({ row: def, draft: true })
+  )
 }
