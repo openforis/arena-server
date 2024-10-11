523bcaff9b3ac62fe499682f02ccbe0688a1f5e5
Stefano Ricci <SteRiccio@users.noreply.github.com>
2024-10-11 10:58:43 +0200

    User auth group fetch: include props

--- src/db/table/schemaPublic/authGroupUser.ts
+++ src/db/table/schemaPublic/authGroupUser.ts
@@ -4,6 +4,7 @@ import { TableSchemaPublic } from './tableSchemaPublic'
 export class TableAuthGroupUser extends TableSchemaPublic {
   readonly userUuid: Column = new Column(this, 'user_uuid', ColumnType.uuid)
   readonly groupUuid: Column = new Column(this, 'group_uuid', ColumnType.uuid)
+  readonly props: Column = new Column(this, 'props', ColumnType.jsonb)
 
   constructor() {
     super('auth_group_user')
