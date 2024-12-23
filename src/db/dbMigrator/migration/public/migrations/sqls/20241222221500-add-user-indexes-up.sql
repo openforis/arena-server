cccb1f14bee507d5dc42b92fa0ddf9bc3dd28909
Stefano Ricci <SteRiccio@users.noreply.github.com>
2024-12-23 11:57:54 +0100

    Revert "added indexes to tables referencing user table (#77)"
    This reverts commit a83d9e83efe2654b9c0c0b85fcb54c5f995fde10.

--- src/db/dbMigrator/migration/public/migrations/sqls/20241222221500-add-user-indexes-up.sql
+++
@@ -1,6 +0,0 @@
-CREATE INDEX user_access_request_modified_by_idx ON user_access_request(modified_by);
-
-CREATE INDEX user_invitation_user_idx ON user_invitation(user_uuid);
-
-CREATE INDEX user_invitation_invited_by_idx ON user_invitation(invited_by);
-
