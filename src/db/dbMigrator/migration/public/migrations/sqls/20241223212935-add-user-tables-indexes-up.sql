4a53473e8bc66e9c7ec5985bc63cb9fc652d0b7e
Stefano Ricci <SteRiccio@users.noreply.github.com>
2024-12-23 22:57:08 +0100

    added auth_group_user_user_idx index

--- src/db/dbMigrator/migration/public/migrations/sqls/20241223212935-add-user-tables-indexes-up.sql
+++ src/db/dbMigrator/migration/public/migrations/sqls/20241223212935-add-user-tables-indexes-up.sql
@@ -1,3 +1,5 @@
+CREATE INDEX IF NOT EXISTS auth_group_user_user_idx ON auth_group_user(user_uuid);
+
 CREATE INDEX IF NOT EXISTS user_access_request_modified_by_idx ON user_access_request(modified_by);
 
 CREATE INDEX IF NOT EXISTS user_invitation_invited_by_idx ON user_invitation(invited_by);
