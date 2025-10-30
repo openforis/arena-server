9f0cf53b32e3db244f37faf510f492888b9ff98d
SteRiccio <1219739+SteRiccio@users.noreply.github.com>
2025-10-30 11:43:07 +0100

    added user_refresh_token table

---
+++ src/db/dbMigrator/migration/public/migrations/sqls/20251030103141-add-table-user-refresh-token-up.sql
@@ -0,0 +1,16 @@
+CREATE TABLE
+  user_refresh_token
+(
+  uuid          uuid        NOT NULL DEFAULT uuid_generate_v4(),
+  user_uuid     uuid        NOT NULL,
+  token         VARCHAR     NOT NULL,
+  props         jsonb       NOT NULL DEFAULT '{}'::jsonb,
+  date_created  TIMESTAMP   NOT NULL DEFAULT (now() AT TIME ZONE 'UTC'),
+  expires_at    TIMESTAMP   NOT NULL DEFAULT (now() AT TIME ZONE 'UTC'),
+  revoked       BOOLEAN     NOT NULL DEFAULT FALSE,
+
+  PRIMARY KEY (uuid),
+  CONSTRAINT user_refresh_token_user_fk FOREIGN KEY (user_uuid) REFERENCES "user" ("uuid") ON DELETE CASCADE
+);
+
+CREATE INDEX IF NOT EXISTS user_refresh_token_user_idx ON user_refresh_token(user_uuid);
