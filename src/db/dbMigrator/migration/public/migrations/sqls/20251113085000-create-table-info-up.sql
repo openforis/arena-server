ad4c5a5adb49d197eb789cbe87617df4bfe5eb92
SteRiccio <1219739+SteRiccio@users.noreply.github.com>
2025-11-13 09:59:46 +0100

    add "info" table to db

---
+++ src/db/dbMigrator/migration/public/migrations/sqls/20251113085000-create-table-info-up.sql
@@ -0,0 +1,9 @@
+
+CREATE TABLE
+  "info"
+(
+
+  key_name       VARCHAR(50) PRIMARY KEY,
+  key_value      TEXT        NOT NULL,
+  modified_date  TIMESTAMP   NOT NULL DEFAULT (now() AT TIME ZONE 'UTC')
+);
