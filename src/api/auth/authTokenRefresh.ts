9d1318799195b07933568251b8cb724640b91358
SteRiccio <1219739+SteRiccio@users.noreply.github.com>
2026-01-08 10:40:55 +0100

    refresh token endpoint: catch error

--- src/api/auth/authTokenRefresh.ts
+++ src/api/auth/authTokenRefresh.ts
@@ -6,24 +6,29 @@ import { extractRefreshTokenProps, jwtRefreshTokenCookieName, setRefreshTokenCoo
 
 export const AuthTokenRefresh: ExpressInitializer = {
   init: (express): void => {
-    express.post(ApiEndpoint.auth.tokenRefresh(), async (req, res) => {
-      const refreshToken = req.cookies?.[jwtRefreshTokenCookieName]
+    express.post(ApiEndpoint.auth.tokenRefresh(), async (req, res, next) => {
+      try {
+        const refreshToken = req.cookies?.[jwtRefreshTokenCookieName]
 
-      const serviceRegistry = ServiceRegistry.getInstance()
-      const userRefreshTokenService: UserAuthTokenService = serviceRegistry.getService(ServiceType.userAuthToken)
+        const serviceRegistry = ServiceRegistry.getInstance()
+        const userRefreshTokenService: UserAuthTokenService = serviceRegistry.getService(ServiceType.userAuthToken)
 
-      const authTokenRotationResult = await userRefreshTokenService.rotateTokens({
-        refreshToken,
-        refreshTokenProps: extractRefreshTokenProps({ req }),
-      })
-      if (!authTokenRotationResult) {
-        return res.status(401).json({ message: 'Invalid or revoked refresh token' })
+        const authTokenRotationResult = await userRefreshTokenService.rotateTokens({
+          refreshToken,
+          refreshTokenProps: extractRefreshTokenProps({ req }),
+        })
+        if (!authTokenRotationResult) {
+          res.status(401).json({ message: 'Invalid or revoked refresh token' })
+          return
+        }
+        const { authToken, refreshToken: newRefreshToken } = authTokenRotationResult
+
+        setRefreshTokenCookie({ res, refreshToken: newRefreshToken })
+
+        res.status(200).json({ authToken: authToken.token, message: 'Token refreshed successfully' })
+      } catch (error: any) {
+        next(error)
       }
-      const { authToken, refreshToken: newRefreshToken } = authTokenRotationResult
-
-      setRefreshTokenCookie({ res, refreshToken: newRefreshToken })
-
-      return res.status(200).json({ authToken: authToken.token, message: 'Token refreshed successfully' })
     })
   },
 }
