a6140ff2717de3f5423e3d26b39536ea0706e402
SteRiccio <1219739+SteRiccio@users.noreply.github.com>
2026-01-21 10:33:37 +0100

    added temp auth token generation endpoint

--- src/api/auth/index.ts
+++ src/api/auth/index.ts
@@ -3,10 +3,12 @@ import { Express } from 'express'
 import { ExpressInitializer } from '../../server'
 import { AuthLogin } from './login'
 import { AuthTokenRefresh } from './authTokenRefresh'
+import { AuthTempToken } from './authTempToken'
 
 export const AuthApi: ExpressInitializer = {
   init: (express: Express): void => {
     AuthLogin.init(express)
     AuthTokenRefresh.init(express)
+    AuthTempToken.init(express)
   },
 }
