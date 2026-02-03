7609cfec998fcc51e3f4939f75e884b2a652045d
Stefano Ricci <1219739+SteRiccio@users.noreply.github.com>
2026-02-03 14:32:30 +0100

    Added express-rate-limit to limit max requests per IP address (#113)
    * added express-rate-limit to limit max requests per IP address
    
    * updated dependencies
    
    * code cleanup
    
    * added "trust proxy" setting
    
    * limit rate only for /auth urls

---
+++ src/server/middleware/rateLimit.ts
@@ -0,0 +1,36 @@
+import { Express } from 'express'
+
+import rateLimit from 'express-rate-limit'
+import { ExpressInitializer } from '../expressInitializer'
+import { ProcessEnv } from '../../processEnv'
+
+const rateLimitWindowMs = 15 * 60 * 1000 // 15 minutes
+const maxRequestsPerWindow = 100 // limit each IP to 100 requests per windowMs
+const rateLimitedPaths = ['/auth']
+
+export const RateLimitMiddleware: ExpressInitializer = {
+  init(express: Express): void {
+    const limiter = rateLimit({
+      windowMs: rateLimitWindowMs,
+      max: maxRequestsPerWindow,
+      message: {
+        status: 429,
+        message: 'Too many requests, please try again later.',
+      },
+    })
+
+    for (const rateLimitedPath of rateLimitedPaths) {
+      express.use(rateLimitedPath, limiter)
+    }
+
+    if (ProcessEnv.useHttps) {
+      // running behind a proxy (e.g. Heroku, AWS ELB, Nginx) - trust the X-Forwarded-* headers
+      const proxies = 1 // number of proxies between user and server
+      express.set('trust proxy', proxies)
+    }
+    // Endpoint to get client IP address (for testing purposes)
+    express.get('/ip', (request, response) => {
+      response.send(request.ip)
+    })
+  },
+}
