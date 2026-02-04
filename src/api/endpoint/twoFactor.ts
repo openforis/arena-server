69e708de2ff5461b4cf888794a94e7a7e83ee6d8
Stefano Ricci <SteRiccio@users.noreply.github.com>
2026-02-04 23:04:04 +0100

    fixed user 2fa device fetch api path

--- src/api/endpoint/twoFactor.ts
+++ src/api/endpoint/twoFactor.ts
@@ -3,7 +3,7 @@ import { getApiPath } from './common'
 export const twoFactor = {
   devices: (): string => getApiPath('2fa', 'devices'),
   devicesCount: (): string => getApiPath('2fa', 'devices', 'count'),
-  device: (): string => getApiPath('2fa', 'device'),
+  device: (): string => getApiPath('2fa', 'device', ':deviceUuid'),
   addDevice: (): string => getApiPath('2fa', 'device', 'add'),
   verifyDevice: (): string => getApiPath('2fa', 'device', 'verify'),
   removeDevice: (): string => getApiPath('2fa', 'device', 'remove'),
