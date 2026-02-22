53b9e8d1cc3f35c8167325ef58cd9425ac06e7c8
Stefano Ricci <SteRiccio@users.noreply.github.com>
2026-02-22 22:28:39 +0100

    improved node i_id update

--- src/db/dbMigrator/migration/survey/migrations/sqls/20260217085512-alter-table-node-add-iid-up.sql
+++ src/db/dbMigrator/migration/survey/migrations/sqls/20260217085512-alter-table-node-add-iid-up.sql
@@ -13,29 +13,30 @@ CREATE INDEX IF NOT EXISTS node_id_idx
 CREATE INDEX IF NOT EXISTS node_record_uuid_id_idx 
     ON node (record_uuid, id);
 
-DO $$
-DECLARE
-    row_count INT;
-BEGIN
-    LOOP
-        -- Update 50,000 rows at a time
-        UPDATE node
-        SET i_id = ranked.i_id
-        FROM (
-            SELECT id, ROW_NUMBER() OVER (PARTITION BY record_uuid ORDER BY id) as i_id
-            FROM node
-            WHERE i_id IS NULL -- Only target rows not yet processed
-            LIMIT 500000
-        ) AS ranked
-        WHERE node.id = ranked.id;
+-- Update node i_id START
 
-        GET DIAGNOSTICS row_count = ROW_COUNT;
-        EXIT WHEN row_count = 0;
-        
-        COMMIT; -- Finalize this chunk so disk space can be reclaimed
-        RAISE NOTICE 'Updated 500,000 rows...';
-    END LOOP;
-END $$;
+CREATE UNLOGGED TABLE temp_node_ranking AS
+SELECT 
+    id, 
+    ROW_NUMBER() OVER (PARTITION BY record_uuid ORDER BY id) AS new_i_id
+FROM node
+ORDER BY id;
+
+CREATE INDEX idx_temp_node_id ON temp_node_ranking (id);
+ANALYZE temp_node_ranking;
+
+SET LOCAL session_replication_role = 'replica';
+
+SET LOCAL work_mem = '512MB';
+
+UPDATE node
+SET i_id = t.new_i_id
+FROM temp_node_ranking t
+WHERE node.id = t.id;
+
+DROP TABLE temp_node_ranking;
+
+-- Update node i_id END
 
 ALTER TABLE node
 	ALTER COLUMN i_id SET NOT NULL;
@@ -62,78 +63,6 @@ SET meta = jsonb_set(
 	)
 )
 WHERE n.meta ? 'h';
-
--- Update activity_log content to use i_id/p_i_id and drop meta.h
-UPDATE activity_log AS al
-SET content = updated.content
-FROM (
-	SELECT al.id,
-        COALESCE(
-            jsonb_set(
-                jsonb_set(
-                    jsonb_set(
-                        (
-                            CASE
-                                WHEN al.content ? 'meta'
-                                    THEN jsonb_set(al.content, '{meta}', (al.content->'meta') - 'h')
-                                ELSE al.content
-                            END
-                        ) - 'parentUuid',
-                        '{iId}',
-                        to_jsonb(n.i_id),
-                        true
-                    ),
-                    '{pIId}',
-                    to_jsonb(n.p_i_id),
-                    true
-                ),
-                '{recordUuid}',
-                to_jsonb(n.record_uuid),
-                true
-            ),
-            al.content -- Fallback to original content if anything fails
-		) AS content
-	FROM activity_log AS al
-	JOIN node AS n ON n.uuid = (al.content->>'uuid')::uuid
-	WHERE al.type IN ('nodeCreate', 'nodeDelete', 'nodeValueUpdate')
-) AS updated
-WHERE al.id = updated.id;
-
-DROP VIEW IF EXISTS activity_log_user_aggregate;
-CREATE VIEW activity_log_user_aggregate AS
-SELECT
-    DISTINCT ON (
-        date_created::date,
-        user_uuid,
-        type,
-        content_uuid,
-        content_key,
-        content_record_uuid,
-        content_i_id
-    )
-    id,
-    date_created,
-    user_uuid,
-    type,
-    (content->>'uuid')::uuid as content_uuid,
-    content->>'key' as content_key,
-    -- node related fields
-    (content->>'recordUuid')::uuid as content_record_uuid,
-    (content->>'iId')::integer as content_i_id,
-    content
-FROM
-    activity_log
-WHERE
-    NOT system
-ORDER BY
-    date_created::date DESC,
-    user_uuid,
-    type,
-    content_uuid,
-    content_key,
-    content_record_uuid,
-    content_i_id,
-    date_created DESC;
 
 -- Add a unique constraint on the combination of record_uuid and i_id
 ALTER TABLE node
