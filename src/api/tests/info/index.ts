042ac1040ea064770bf603abf62dc8b6d32098a4
SteRiccio <1219739+SteRiccio@users.noreply.github.com>
2026-03-03 11:22:10 +0100

    code cleanup (fixed SonarCloud issue)

--- src/api/tests/info/index.ts
+++ src/api/tests/info/index.ts
@@ -6,7 +6,7 @@ declare global {
   var api: ApiTest
 }
 
-export default (): void =>
+const initInfoApiTests = (): void =>
   describe(`Info ${ApiEndpoint.info.info()}`, () => {
     test('Get app info', async () => {
       const { body } = await globalThis.api.get(ApiEndpoint.info.info()).expect(200)
@@ -14,5 +14,8 @@ export default (): void =>
       expect(body).toBeDefined()
       expect(body.applicationVersion).toBe(ProcessEnv.applicationVersion)
       expect(body.fileUploadLimit).toBe(ProcessEnv.fileUploadLimit)
+      expect(body.experimentalFeatures).toBe(ProcessEnv.experimentalFeatures)
     })
   })
+
+export default initInfoApiTests
