d7b6149fcc12e1c24682e82f54e0f96bc228937f
Stefano Ricci <SteRiccio@users.noreply.github.com>
2026-03-10 22:45:23 +0100

    added record validation migration

--- src/db/dbMigrator/migration/survey/migrations/sqls/20260217085512-alter-table-node-add-iid-up.sql
+++ src/db/dbMigrator/migration/survey/migrations/sqls/20260217085512-alter-table-node-add-iid-up.sql
@@ -66,6 +66,49 @@ SET meta = jsonb_set(
 )
 WHERE n.meta ? 'h';
 
+-- Update record validation fields to replace UUID references with i_id references
+
+UPDATE record r
+SET validation = jsonb_set(
+    r.validation, 
+    '{fields}', 
+    updated_fields.new_fields
+)
+FROM (
+    SELECT 
+        r_inner.id,
+        jsonb_object_agg(
+            CASE 
+                -- Case 1: childrenCount_NODEUUID_NODEDEFUUID
+                WHEN fields.key LIKE 'childrenCount_%' THEN 
+                    'childrenCount_' || COALESCE(n.i_id::text, split_part(fields.key, '_', 2)) || '_' || split_part(fields.key, '_', 3)
+                
+                -- Case 2: Plain NODEUUID
+                ELSE 
+                    COALESCE(n.i_id::text, fields.key)
+            END,
+            fields.value
+        ) as new_fields
+    FROM record r_inner
+    CROSS JOIN LATERAL jsonb_each(r_inner.validation->'fields') AS fields(key, value)
+    -- Join logic: if prefixed, get 2nd part; if not, get 1st part
+    LEFT JOIN node n ON (
+        CASE 
+            WHEN fields.key LIKE 'childrenCount_%' THEN split_part(fields.key, '_', 2)
+            ELSE fields.key
+        END
+    )::uuid = n.uuid
+    GROUP BY r_inner.id
+) AS updated_fields
+WHERE r.id = updated_fields.id;
+
+-- Create an index on the combination of record_uuid and i_id for faster lookups
+
+CREATE INDEX IF NOT EXISTS node_record_uuid_i_id_idx
+	ON node (record_uuid, i_id);
+
+-- Finally, update the node table constraints to reflect the new primary key and foreign key relationships
+
 ALTER TABLE node
 	DROP CONSTRAINT IF EXISTS node_parent_fk,
 	DROP CONSTRAINT IF EXISTS node_pkey,
@@ -73,6 +116,3 @@ ALTER TABLE node
 	ADD PRIMARY KEY (record_uuid, i_id),
 	ADD CONSTRAINT node_parent_fk FOREIGN KEY (record_uuid, p_i_id) REFERENCES "node" (record_uuid, i_id) ON DELETE CASCADE;
 
--- Create an index on the combination of record_uuid and i_id for faster lookups
-CREATE INDEX IF NOT EXISTS node_record_uuid_i_id_idx
-	ON node (record_uuid, i_id);
